f1bacf8ad2b69c2e842a979d71184f03c42506b5
Yaroslav Polyakov <yaroslaff@gmail.com>
2014-10-04 22:28:49 +0700

    changed bitbucket address

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import setup
 setup(name='evalidate',
       version='0.1',
       description='Eval code validator',
-      url='http://github.com/storborg/funniest',
+      url='https://bitbucket.org/yaroslaff/evalidate',
       author='Yaroslav Polyakov',
       author_email='xenon@sysattack.com',
       license='MIT',
-      packages=['funniest'],
+      packages=['evalidate'],
       zip_safe=False)
 

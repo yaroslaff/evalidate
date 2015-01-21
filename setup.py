7e90c7aa7dccb9a68e0a167727847a0ff807b9f7
xenon <yaroslaff@gmail.com>
2015-01-21 17:58:07 +0600

    changed version number

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='evalidate',
-      version='0.5',
+      version='0.6',
       description='Eval code validator',
       url='http://evalidate.readthedocs.org/',
       author='Yaroslav Polyakov',

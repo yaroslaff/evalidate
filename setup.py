20d41bfda7a2ccd699dba0057a763529ecb44fd2
Yaroslav Polyakov <yaroslaff@gmail.com>
2018-10-02 04:17:43 +0700

    setup.py has markdown long description now

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
 from setuptools import setup
+import os
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(name='evalidate',
-      version='0.7.2',
-      description='Eval code validator',
+      version='0.7.4',
       url='http://evalidate.readthedocs.org/',
       author='Yaroslav Polyakov',
       author_email='xenon@sysattack.com',
       license='MIT',
       packages=['evalidate'],
+      description = 'Validation and secure evaluation untrusted python expressions',
+      long_description = read('README.md'),
+      long_description_content_type='text/markdown',
       zip_safe=False)
 

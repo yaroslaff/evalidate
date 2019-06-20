1593570f624bdb7cc6ff672e50ff22f294945ab5
Yaroslav Polyakov <yaroslaff@gmail.com>
2019-06-21 01:11:07 +0700

    no changes in src. fixed link to bitbucket repo and setup.py now backward-compatible with python 3.5

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 import os
 
 def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+    return open(os.path.join(os.path.dirname(__file__), fname), encoding='utf-8').read()
 
 setup(name='evalidate',
-      version='0.7.6',
-      url='http://evalidate.bitbucket.org/',
+      version='0.7.7',
+      url='http://bitbucket.org/yaroslaff/evalidate',
       author='Yaroslav Polyakov',
       author_email='xenon@sysattack.com',
       license='MIT',
